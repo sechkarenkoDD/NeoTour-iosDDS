bccd20b70a4372e6a240e82315b9df9b9c2c762a
Dmitry Sechkarenko <sarp69@icloud.com>
2024-04-10 00:27:27 +0300

    setup SnapKit layout Onboarding screen adding fonts

---
+++ NeoTour-iosDDS/Extension/UIColor.swift
@@ -0,0 +1,8 @@
+//
+//  UIColor.swift
+//  Pods
+//
+//  Created by Dmitry on 09.04.2024.
+//
+
+import Foundation
---
+++ NeoTour-iosDDS/MainView.swift
@@ -0,0 +1,12 @@
+//
+//  MainView.swift
+//
+//
+//  Created by Dmitry on 10.04.2024.
+//
+
+import UIKit
+
+class MainView: UICollectionViewController {
+    
+}
