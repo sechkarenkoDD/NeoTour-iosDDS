137143e8f000ae8f314bb87e8b13d257237bd4f9
Dmitry Sechkarenko <sarp69@icloud.com>
2024-04-09 19:09:46 +0300

    Initial Commit

---
+++ NeoTour-iosDDS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NeoTour-iosDDS
+//
+//  Created by Dmitry on 09.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
